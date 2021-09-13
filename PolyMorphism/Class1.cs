008c6d9f135cb0ec7c011f110d80d4ae36751eb7
JZN-37 <89829197+JZN-37@users.noreply.github.com>
2021-09-13 16:42:26 +0530

    Day 3

---
+++ PolyMorphism/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism
+{
+    class RefAndOutDemo
+    {
+        public static void Main(string[] values)
+        {
+            //OutDemo();
+            RefDemo();
+            Console.Read();
+        }
+
+        public static void RefDemo()
+        {
+            string str1 = "Geo";
+            SetValue(ref str1);
+            Console.WriteLine(str1);
+        }
+        static void SetValue(ref string str)
+        {
+            str = "JOoo";
+        }
+
+
+        public static void OutDemo()
+        {
+            int result;
+            Add(out result);
+            Console.WriteLine(result);
+        }
+        public static bool Add(out int a)
+        {
+            a = 20;
+            a = a + a;
+            return true;
+        }      
+    }
+}
---
+++ PolyMorphism/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism
+{
+    enum role
+    {
+        Developer=2, Delivery_Manager=5
+    }
+    class User
+    {
+        public role role { get; set; }
+        public string name { get; } //read only
+        const int age = 20; //instance constant
+        public User(string name)
+        {
+            this.name = name;
+            //Console.WriteLine("user");
+        }
+        public void Enter()
+        {
+            //Console.WriteLine("user");
+        }
+    }
+    class Employee : User
+    {
+        public Employee(string name):base(name)
+        {
+            Console.WriteLine("emp constructor");
+        }
+        /*
+        public static string h="hello";
+        static Employee()
+        {
+            //h = "hello";
+            Console.WriteLine("static emp");
+        }
+        */
+        public new void Enter()   //over riding a method,
+        {
+            Console.WriteLine("employee");
+        }
+    }
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("Josen");
+            e1.Enter();
+            //Console.WriteLine(Employee.h);
+            Console.WriteLine(e1.name);
+
+            e1.role = role.Delivery_Manager;
+            Console.WriteLine((int)e1.role);
+            string strRole = role.Delivery_Manager.ToString();
+            Console.WriteLine(strRole);
+
+            //User u1 = new User();
+            //u1.Enter();
+            
+            Console.Read();
+        }
+    }
+}
